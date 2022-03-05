7cf010e50367f92349a491775d8e0f2f8a985f95
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-03-05 17:28:14 +0700

    create book and images services

--- src/services/books.service.ts
+++ src/services/books.service.ts
@@ -4,18 +4,29 @@ import { FindConditions, Repository } from 'typeorm';
 import { BookEntity } from 'entity/books.entity';
 import { CreateBookDto } from 'dto/create.books.dto';
 import { FilterBookDto } from 'dto/filter.books.dto';
+import { ResponseBook } from 'dto/response.books.dto';
 
 @Injectable()
 export class BookService {
        constructor(@InjectRepository(BookEntity) private bookRepository: Repository<BookEntity>) { }
 
-       async getBooks(filterBookDto: FilterBookDto): Promise<BookEntity[]> {
+       async getBooks(filterBookDto: FilterBookDto): Promise<any> {
+              // default return is <BookEntity[]> 
               let filter: FindConditions<BookEntity> = {}
               if (filterBookDto.title) {
                      filter = { title: filterBookDto.title }
               }
               const books = await this.bookRepository.find(filter)
-              return books
+              const response = {
+                     status: 200,
+                     method: "GET",
+                     api_version: "v1",
+                     data: {
+                            message: "Successfully Get All Books",
+                            data: books
+                     }
+              }
+              return response
        }
 
        async createBooks(bookData: CreateBookDto): Promise<BookEntity> {
@@ -25,6 +36,51 @@ export class BookService {
        }
 
        async findOne(id: number): Promise<BookEntity> {
-              return this.bookRepository.findOne(id)
+              try {
+                     const book = await this.bookRepository.findOne(id)
+                     return book
+              } catch (error) {
+                     throw error
+              }
+       }
+
+       async updateBooks(id: number, bookData: CreateBookDto): Promise<BookEntity> {
+              await this.bookRepository.update(id, bookData)
+              return bookData
+       }
+
+       async deleteBooks(id: number): Promise<ResponseBook> {
+              const books = await this.bookRepository.findOne(id)
+              const bookId = await this.bookRepository.delete(id)
+              return {
+                     status: 200,
+                     api_version: "v1",
+                     method: "DELETE",
+                     data: {
+                            message: "Successfully Delete Book",
+                            data: books
+                     }
+              }
+       }
+
+       bookControllerv2() {
+              const response = {
+                     status: 200,
+                     method: "GET",
+                     api_version: "v2",
+                     data: {
+                            message: "Successfully Get All Books",
+                            data: [
+                                   {
+                                          id: 22,
+                                          title: "React Design Pattern",
+                                          author: "Ferdian",
+                                          publisher: "Ferdian Corporation",
+                                          description: "Book every programmer must have"
+                                   }
+                            ]
+                     }
+              }
+              return response
        }
 }
---
+++ src/services/images.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { mockImagesData } from "apis"
+
+@Injectable()
+export class ImagesService {
+       async getImages(): Promise<Object> {
+              return {
+                     status: 200,
+                     method: "GET",
+                     api_version: "v1",
+                     data: {
+                            message: "Successfully Get All Images",
+                            data: mockImagesData
+                     }
+              }
+       }
+}
