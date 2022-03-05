ec50bef3bc05196d2e02bb8db0b1874695c98bd1
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-03-05 19:54:26 +0700

    apply book middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,22 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from 'app.controller';
 import { AppService } from 'app.service';
+import { CreateBooksMiddlewares, DeleteBooksMiddleware, GetBooksMiddleware, UpdateBooksMiddleware } from 'common/middlewares/books.middleware';
 import { BooksModule } from 'modules/books.module';
 import { ImagesModule } from 'modules/images.module';
 
 @Module({
-  imports: [BooksModule, TypeOrmModule.forRoot(), ImagesModule],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [BooksModule, TypeOrmModule.forRoot(), ImagesModule],
+	controllers: [AppController],
+	providers: [AppService],
 })
 
-export class AppModule { }
+export class AppModule implements NestModule {
+	configure(consumer: MiddlewareConsumer) {
+		consumer.apply(CreateBooksMiddlewares).forRoutes({ path: "v1/books", method: RequestMethod.POST })
+		consumer.apply(GetBooksMiddleware).forRoutes({ path: "v1/books/*", method: RequestMethod.GET })
+		consumer.apply(UpdateBooksMiddleware).forRoutes({ path: "v1/books/*", method: RequestMethod.PUT })
+		consumer.apply(DeleteBooksMiddleware).forRoutes({ path: "v1/books/*", method: RequestMethod.DELETE })
+	}
+}
